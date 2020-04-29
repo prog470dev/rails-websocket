3050916e746dbc9c9fdbdef938c43bf6c6bac500
prog470dev <tshimizudev4@gmail.com>
2020-04-29 16:18:18 +0900

    impl sennding message from client

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Actioncable from "actioncable";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+    };
+    this.handleClick = this.handleClick.bind(this);
+
+    // websocketの設定
+    this.endpoint = "ws:localhost:3000/cable";
+    this.cable = Actioncable.createConsumer(this.endpoint);
+    // 購読するチャネルの設定
+    this.chatChannel = this.cable.subscriptions.create(
+      {
+        // チャネル(=Rails側のチャネルクラス名)の指定
+        channel: "BroadcastChannel",
+      },
+      {
+        // チャネルとの接続が確立した時に実行される処理
+        connected: () => {
+          console.log("connected.");
+        },
+        // チャネルからメッセージを購読した時に実行される処理
+        received: (data) => {
+          this.setState({
+            messages: [...this.state.messages, data],
+          });
+        },
+      }
+    );
+  }
+
+  handleClick() {
+    // Rails側のチャネルクラスで定義されたメソッドを指定することでメッセージ送信
+    // 送信データは object（ハッシュ形式）
+    this.chatChannel.perform("speak", {
+      message: "hoge",
+    });
+  }
+
+  render() {
+    const items = this.state.messages.map((message, i) => {
+      return <li key={i}>{message}</li>;
+    });
+    return (
+      <div>
+        <button onClick={this.handleClick}>message</button>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- rails-app/app/channels/broadcast_channel.rb
+++ rails-app/app/channels/broadcast_channel.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class BroadcastChannel < ApplicationCable::Channel
+  # クライアントの購読が確立したときに呼ばれるメソッド
   def subscribed
+    # クライアントとストリームの紐付け
     stream_from 'broadcast_channel'
   end
 
+  # クライアントから指定されるメソッド
   def speak(data)
     ActionCable.server.broadcast('broadcast_channel', data['message'])
   end
